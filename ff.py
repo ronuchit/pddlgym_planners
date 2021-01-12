7ca63babe603a4578018e14f0e0f2ebf8febe4d3
Bruno <bruno.ibanezlopez@gmail.com>
2021-01-12 18:00:47 +0100

    Fixed bug (copy of FD planner as FF)

--- ff.py
+++ ff.py
@@ -1,104 +1,82 @@
-"""Fast-downward planner.
-http://www.fast-downward.org/ObtainingAndRunningFastDownward
+"""Fast-forward planner.
+https://fai.cs.uni-saarland.de/hoffmann/ff.html
 """
 
 import re
 import os
 import sys
-import subprocess
-import tempfile
 from pddlgym_planners.pddl_planner import PDDLPlanner
 from pddlgym_planners.planner import PlanningFailure
 
-FD_URL = "https://github.com/ronuchit/downward.git"
+FF_URL = "https://fai.cs.uni-saarland.de/hoffmann/ff/FF-v2.3.tgz"
+FF_MAC_URL = "git@github.com:ronuchit/FF.git"
 
 
-class FD(PDDLPlanner):
-    """Fast-downward planner.
+class FF(PDDLPlanner):
+    """Fast-forward planner.
     """
-    def __init__(self, alias_flag="--alias seq-opt-lmcut", final_flags=""):
+    def __init__(self):
         super().__init__()
         dirname = os.path.dirname(os.path.realpath(__file__))
-        self._exec = os.path.join(dirname, "FD/fast-downward.py")
-        print("Instantiating FD", end=' ')
-        if alias_flag:
-            print("with", alias_flag, end=' ')
-        if final_flags:
-            print("with", final_flags, end=' ')
-        print()
-        self._alias_flag = alias_flag
-        self._final_flags = final_flags
+        self._exec = os.path.join(dirname, "FF-v2.3/ff")
+        print("Instantiating FF")
         if not os.path.exists(self._exec):
-            self._install_fd()
+            self._install_ff()
 
     def _get_cmd_str(self, dom_file, prob_file, timeout):
-        sas_file = tempfile.NamedTemporaryFile(delete=False).name
         timeout_cmd = "gtimeout" if sys.platform == "darwin" else "timeout"
-        cmd_str = "{} {} {} {} --sas-file {} {} {} {}".format(
-            timeout_cmd, timeout, self._exec, self._alias_flag, sas_file,
-            dom_file, prob_file, self._final_flags)
+        cmd_str = "{} {} {} -o {} -f {}".format(
+            timeout_cmd, timeout, self._exec, dom_file, prob_file)
         return cmd_str
 
-    def _cleanup(self):
-        """Run FD cleanup
-        """
-        cmd_str = "{} --cleanup".format(self._exec)
-        subprocess.getoutput(cmd_str)
-
     def _output_to_plan(self, output):
-        # Technically this is number of evaluated states which is always
-        # 1+number of expanded states, but we report evaluated for consistency
-        # with FF.
-        num_node_expansions = re.findall(r"evaluated (\d+) state", output.lower())
-        plan_length = re.findall(r"plan length: (\d+) step", output.lower())
-        plan_cost = re.findall(r"] plan cost: (\d+)", output.lower())
-        search_time = re.findall(r"] search time: (\d+\.\d+)", output.lower())
-        total_time = re.findall(r"] total time: (\d+\.\d+)", output.lower())
+        num_node_expansions = re.findall(r"evaluating (.+) states", output.lower())
+        total_time = re.findall(r"(\d+\.\d+) seconds total time", output.lower())
+        search_time = re.findall(r"(\d+\.\d+) seconds total time", output.lower())
         if "num_node_expansions" not in self._statistics:
             self._statistics["num_node_expansions"] = 0
         if len(num_node_expansions) == 1:
             assert int(num_node_expansions[0]) == float(num_node_expansions[0])
             self._statistics["num_node_expansions"] += int(
                 num_node_expansions[0])
-        if len(search_time) == 1:
-            try:
-                search_time_float = float(search_time[0])
-                self._statistics["search_time"] = search_time_float
-            except:
-                raise PlanningFailure("Error on output's search time format: {}".format(search_time[0]))
-        if len(search_time) == 1:
-            try:
+        if "found legal plan" in output:
+            plan_length = re.findall(r"(\d+):", output.lower())
+            self._statistics["plan_length"] = len(plan_length)
+        if len(total_time) == 1:
+            try: 
                 total_time_float = float(total_time[0])
                 self._statistics["total_time"] = total_time_float
             except:
                 raise PlanningFailure("Error on output's total time format: {}".format(total_time[0]))
-        if len(plan_length) == 1:
-            try:
-                plan_length_int = int(plan_length[0])
-                self._statistics["plan_length"] = plan_length_int
+        if len(search_time) == 1:
+            try: 
+                search_time_float = float(search_time[0])
+                self._statistics["search_time"] = search_time_float
             except:
-                raise PlanningFailure("Error on output's plan length format: {}".format(plan_length[0]))
-        if len(plan_cost) == 1:
-            try:
-                plan_cost_int = int(plan_cost[0])
-                self._statistics["plan_cost"] = plan_cost_int
-            except:
-                raise PlanningFailure("Error on output's plan cost format: {}".format(plan_cost[0]))
-        if "Solution found!" not in output:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+                raise PlanningFailure("Error on output's search time format: {}".format(search_time[0]))
+        if "goal can be simplified to FALSE" in output:
+            raise PlanningFailure("Plan not found with FF! Error: {}".format(
                 output))
-        if "Plan length: 0 step" in output:
-            return []
-        
-        fd_plan = re.findall(r"(.+) \(\d+?\)", output.lower())
-        if not fd_plan:
-            raise PlanningFailure("Plan not found with FD! Error: {}".format(
+        if "unsolvable" in output:
+            raise PlanningFailure("Plan not found with FF! Error: {}".format(
                 output))
-        return fd_plan
+        ff_plan = re.findall(r"\d+?: (.+)", output.lower())
+        if not ff_plan:
+            raise PlanningFailure("Plan not found with FF! Error: {}".format(
+                output))
+        if ff_plan[-1] == "reach-goal":
+            ff_plan = ff_plan[:-1]
+        return ff_plan
 
-    def _install_fd(self):
+    def _install_ff(self):
         loc = os.path.dirname(self._exec)
-        # Install and compile FD.
-        os.system("git clone {} {}".format(FD_URL, loc))
-        os.system("cd {} && ./build.py && cd -".format(loc))
+        if sys.platform == "darwin":
+            # Install FF patched for Mac.
+            os.system("git clone {} {}".format(FF_MAC_URL, loc))
+        else:
+            # Install FF directly from official website.
+            os.system("curl {} --output {}.tgz".format(FF_URL, loc))
+            os.system("tar -xzvf {}.tgz".format(loc))
+        # Compile FF.
+        os.system("cd {} && make && cd -".format(loc))
         assert os.path.exists(self._exec)
