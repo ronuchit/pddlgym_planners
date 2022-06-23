1cd67c77de9f07d00a7cb25feb94cd87f3e00724
Tom Silver <tomssilver@gmail.com>
2022-06-23 15:24:55 -0400

    add pddlgym dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='pddlgym_planners',
       version='0.0.1',
-      install_requires=['numpy'],
+      install_requires=['numpy', 'pddlgym'],
       packages=find_packages(),
       include_package_data=True,
 )
